41cdc737896750d39bde9e430a527bb693b4042a
kiwiidb <33457577+kiwiidb@users.noreply.github.com>
2022-01-27 09:53:13 +0100

    Update lightsail-compose.sh

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
+sudo amazon-linux-extras install docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
 
-# install latest version of docker the lazy way
-curl -sSL https://get.docker.com | sh
-
-# make it so you don't need to sudo to run docker commands
-sudo usermod -aG docker ubuntu
-
-# install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-# copy the dockerfile into /srv/docker 
-# if you change this, change the systemd service file to match
-# WorkingDirectory=[whatever you have below]
-mkdir /srv/docker
-curl -o docker-compose.yml https://raw.githubusercontent.com/kiwiidb/plausible-hosting/master/docker-compose.yml
-curl -o docker-compose.caddy-gen.yml https://raw.githubusercontent.com/kiwiidb/plausible-hosting/master/reverse-proxy/docker-compose.caddy-gen.yml
-
-
-# copy in systemd unit file and register it so our compose file runs 
-# on system restart
-# curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/kiwiidb/plausible-hosting/master/docker-compose-app.service
-# systemctl enable docker-compose-app
-
-# start up the application via docker-compose
-docker-compose -f docker-compose.yml -f docker-compose.caddy-gen.yml up -d
+# Make docker auto-start
+sudo chkconfig docker on
+# Install git
+sudo yum install -y git
+# Install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
