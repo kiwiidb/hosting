e6e8b184d01fbbece54e71a27c8d8512af797c65
kiwiidb <33457577+kiwiidb@users.noreply.github.com>
2022-01-27 10:03:01 +0100

    Update lightsail-compose.sh

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -5,8 +5,6 @@ sudo usermod -a -G docker ec2-user
 
 # Make docker auto-start
 sudo chkconfig docker on
-# Install git
-sudo yum install -y git
 # Install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
