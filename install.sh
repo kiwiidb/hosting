aee98271620debdef3f28fa3c3ee6df04fdfc6dd
kiwiidb <kwinten.debacker@hotmail.com>
2022-01-29 12:01:03 +0100

    fix sed scripts

--- install.sh
+++ install.sh
@@ -13,13 +13,13 @@ docker-compose version
 
 # Inject configuration variables
 SECRET_KEY_BASE=$(openssl rand -base64 64 | tr -d '\n')
-sed -i "s/%ADMIN_USER_EMAIL/$ADMIN_USER_EMAIL" plausible-conf.env
-sed -i "s/%ADMIN_USER_NAME/$ADMIN_USER_NAME" plausible-conf.env
-sed -i "s/%ADMIN_USER_PWD/$ADMIN_USER_PWD" plausible-conf.env
-sed -i "s/%HOST/$HOST" plausible-conf.env
-sed -i "s/%SECRET_KEY_BASE/$SECRET_KEY_BASE" plausible-conf.env
+sed -i "s/%ADMIN_USER_EMAIL/$ADMIN_USER_EMAIL/g" plausible-conf.env
+sed -i "s/%ADMIN_USER_NAME/$ADMIN_USER_NAME/g" plausible-conf.env
+sed -i "s/%ADMIN_USER_PWD/$ADMIN_USER_PWD/g" plausible-conf.env
+sed -i "s/%HOST/$HOST/g" plausible-conf.env
+sed -i "s/%SECRET_KEY_BASE/$SECRET_KEY_BASE/g" plausible-conf.env
 
-sed -i "s/%ADMIN_USER_EMAIL/$ADMIN_USER_EMAIL" reverse-proxy/docker-compose.caddy-gen.yml
-sed -i "s/%HOST/$HOST" reverse-proxy/docker-compose.caddy-gen.yml
+sed -i "s/%ADMIN_USER_EMAIL/$ADMIN_USER_EMAIL/g" reverse-proxy/docker-compose.caddy-gen.yml
+sed -i "s/%HOST/$HOST/g" reverse-proxy/docker-compose.caddy-gen.yml
 
 ./start.sh
