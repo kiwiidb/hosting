3c5247de8ec75de354634424c31b3f0752361e4d
kiwiidb <kwinten.debacker@hotmail.com>
2022-01-29 12:15:47 +0100

    fix sed scripts

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ SECRET_KEY_BASE=$(openssl rand -base64 64 | tr -d '\n')
 sed -i "s|%ADMIN_USER_EMAIL|$ADMIN_USER_EMAIL|g" plausible-conf.env
 sed -i "s|%ADMIN_USER_NAME|$ADMIN_USER_NAME|g" plausible-conf.env
 sed -i "s|%ADMIN_USER_PWD|$ADMIN_USER_PWD|g" plausible-conf.env
-sed -i "s|%HOST/$HOST|g" plausible-conf.env
+sed -i "s|%HOST|$HOST|g" plausible-conf.env
 sed -i "s|%SECRET_KEY_BASE|$SECRET_KEY_BASE|g" plausible-conf.env
 
 sed -i "s|%ADMIN_USER_EMAIL|$ADMIN_USER_EMAIL|g" reverse-proxy/docker-compose.caddy-gen.yml
