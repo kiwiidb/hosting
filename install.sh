c1988ffc1d255160718eb471bd3c1ece20648c55
kiwiidb <kwinten.debacker@hotmail.com>
2022-01-29 12:14:16 +0100

    update script

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-sudo amazon-linux-extras install docker -y
-sudo service docker start
-sudo usermod -a -G docker ec2-user
-newgrp docker
-
-# Make docker auto-start
-sudo chkconfig docker on
-# Install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose version
+if docker ps ; then
+	echo "Docker already running"
+else
+	echo "Docker not yet running, installing and starting now"
+	sudo amazon-linux-extras install docker -y
+	sudo service docker start
+	sudo usermod -a -G docker ec2-user
+	newgrp docker
+	# Make docker auto-start
+	sudo chkconfig docker on
+fi
+if docker-compose version ; then
+	echo "docker-compose already installed"
+else
+	# Install docker-compose
+	sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+	docker-compose version
+fi
 
 # Inject configuration variables
 SECRET_KEY_BASE=$(openssl rand -base64 64 | tr -d '\n')
